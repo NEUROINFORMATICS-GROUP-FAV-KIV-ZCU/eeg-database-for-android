adc7ac454d85557f399d618f4314da8e6debe064
Petr Miko <miko.petr@gmail.com>
2012-12-18 08:42:48 +0100

    Fixed recreating progress dialog when view is recreated. If fragment is not found (occurs sometimes), data do not update automatically.

--- src/cz/zcu/kiv/eeg/mobile/base/archetypes/CommonActivity.java
+++ src/cz/zcu/kiv/eeg/mobile/base/archetypes/CommonActivity.java
@@ -1,81 +1,104 @@
 package cz.zcu.kiv.eeg.mobile.base.archetypes;
 
-import android.os.Bundle;
-import cz.zcu.kiv.eeg.mobile.base.R;
-import cz.zcu.kiv.eeg.mobile.base.data.Constants;
-import cz.zcu.kiv.eeg.mobile.base.data.ServiceState;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import cz.zcu.kiv.eeg.mobile.base.R;
+import cz.zcu.kiv.eeg.mobile.base.data.Constants;
+import cz.zcu.kiv.eeg.mobile.base.data.ServiceState;
 
 public class CommonActivity extends Activity {
 
-	protected volatile ProgressDialog progressDialog;
+    protected volatile ProgressDialog progressDialog;
     public static CommonService service;
+    private String progressMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(service != null)
-            service.setActivity(this);
+        synchronized (CommonActivity.class) {
+            if (service != null) {
+                service.setActivity(this);
+
+                if (savedInstanceState != null) {
+                    progressMessage = savedInstanceState.getString("progressMsg");
+                    progressDialog = ProgressDialog.show(CommonActivity.this,
+                            getString(R.string.working), progressMessage, true, false);
+
+                }
+            }
+        }
     }
 
     public void changeProgress(ServiceState state) {
-		changeProgress(state, null);
-	}
+        changeProgress(state, null);
+    }
 
-	public void changeProgress(final ServiceState state, final String message) {
-		new Handler(Looper.getMainLooper()).post(new Runnable() {
-			@Override
-			public void run() {
-				switch (state) {
-				case RUNNING:
-					progressDialog = ProgressDialog.show(CommonActivity.this,
-							getString(R.string.working), message, true, false);
-					break;
-				case INACTIVE:
-				case DONE:
-                    service = null;
-					if (progressDialog != null && progressDialog.isShowing()){
-						progressDialog.dismiss();
+    public void changeProgress(final ServiceState state, final String message) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                switch (state) {
+                    case RUNNING:
+                        progressMessage = message;
+                        progressDialog = ProgressDialog.show(CommonActivity.this,
+                                getString(R.string.working), message, true, false);
+                        break;
+                    case INACTIVE:
+                    case DONE:
+                        synchronized (CommonActivity.class) {
+                            service = null;
+                            progressMessage = null;
+                            if (progressDialog != null && progressDialog.isShowing()) {
+                                progressDialog.dismiss();
+                            }
+                        }
+                        break;
+                    case ERROR:
+                        showAlert(message);
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    public void showAlert(String alert) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(alert).setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
                     }
-					break;
-				case ERROR:
-					showAlert(message);
-				default:
-					break;
-				}
-			}
-		});
-	}
+                });
+        builder.create().show();
+    }
 
-	public void showAlert(String alert) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(alert).setCancelable(false)
-				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				});
-		builder.create().show();
-	}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (service != null && progressDialog != null) {
+            outState.putString("progressMsg", progressMessage);
+        }
+    }
 
-	@Override
-	protected void onPause() {
-		if (progressDialog != null && progressDialog.isShowing()) {
-			progressDialog.dismiss();
-		}
-		super.onPause();
-	}
+    @Override
+    protected void onPause() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+        super.onPause();
+    }
 
-	protected SharedPreferences getCredentials() {
-		return getSharedPreferences(Constants.PREFS_CREDENTIALS, Context.MODE_PRIVATE);
-	}
+    protected SharedPreferences getCredentials() {
+        return getSharedPreferences(Constants.PREFS_CREDENTIALS, Context.MODE_PRIVATE);
+    }
 }
--- src/cz/zcu/kiv/eeg/mobile/base/ui/reservation/AgendaListFragment.java
+++ src/cz/zcu/kiv/eeg/mobile/base/ui/reservation/AgendaListFragment.java
@@ -1,149 +1,155 @@
 package cz.zcu.kiv.eeg.mobile.base.ui.reservation;
 
-import java.util.Calendar;
-
-import cz.zcu.kiv.eeg.mobile.base.R;
-import cz.zcu.kiv.eeg.mobile.base.data.Constants;
-import cz.zcu.kiv.eeg.mobile.base.data.container.Reservation;
-import cz.zcu.kiv.eeg.mobile.base.data.container.ReservationAdapter;
-
 import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.app.FragmentTransaction;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.*;
 import android.view.View.OnClickListener;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import cz.zcu.kiv.eeg.mobile.base.R;
+import cz.zcu.kiv.eeg.mobile.base.archetypes.CommonActivity;
+import cz.zcu.kiv.eeg.mobile.base.data.Constants;
+import cz.zcu.kiv.eeg.mobile.base.data.container.Reservation;
+import cz.zcu.kiv.eeg.mobile.base.data.container.ReservationAdapter;
+
+import java.util.Calendar;
 
 public class AgendaListFragment extends Fragment implements OnClickListener {
 
-	public final static String TAG = AgendaListFragment.class.getSimpleName();
+    public final static String TAG = AgendaListFragment.class.getSimpleName();
 
-	private static int year = -1, month = -1, day = -1;
-	private TextView dateLabel;
+    private static int year = -1, month = -1, day = -1;
+    private TextView dateLabel;
 
-	private final OnDateSetListener dateSetListener = new OnDateSetListener() {
+    private final OnDateSetListener dateSetListener = new OnDateSetListener() {
 
-		@Override
-		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-			AgendaListFragment.year = year;
-			month = monthOfYear;
-			day = dayOfMonth;
-			updateDate();
-			updateData();
-		}
-	};
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            AgendaListFragment.year = year;
+            month = monthOfYear;
+            day = dayOfMonth;
+            updateDate();
+            updateData();
+        }
+    };
 
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
 
-        if(savedInstanceState == null){
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
             ReservationListFragment list = new ReservationListFragment();
             FragmentTransaction ft = getFragmentManager().beginTransaction();
             ft.replace(R.id.reservation_list, list, ReservationListFragment.TAG);
             ft.commit();
         }
-		setHasOptionsMenu(true);
-	}
+        setHasOptionsMenu(true);
+    }
 
-	private void initButtons() {
-		Button chooseDateButton = (Button) getActivity().findViewById(R.id.chooseDateButton);
-		chooseDateButton.setOnClickListener(this);
-	}
+    private void initButtons() {
+        Button chooseDateButton = (Button) getActivity().findViewById(R.id.chooseDateButton);
+        chooseDateButton.setOnClickListener(this);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.reser_agenda, container, false);
-	}
+    }
 
-	@Override
-	public void onActivityCreated(Bundle savedState) {
-		super.onActivityCreated(savedState);
+    @Override
+    public void onActivityCreated(Bundle savedState) {
+        super.onActivityCreated(savedState);
 
         initButtons();
-		initData(savedState);
-		updateDate();
-//		if(!((CommonActivity) getActivity()).progressOn)
-//			updateData();
-	}
+        initData(savedState);
+        updateDate();
+    }
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
         Log.d(TAG, "Saving date...");
-		super.onSaveInstanceState(outState);
-		outState.putInt("year", year);
-		outState.putInt("month", month);
-		outState.putInt("day", day);
-	}
+        super.onSaveInstanceState(outState);
+        outState.putInt("year", year);
+        outState.putInt("month", month);
+        outState.putInt("day", day);
+    }
 
-	private void initData(Bundle savedState) {
+    private void initData(Bundle savedState) {
 
         Calendar c = Calendar.getInstance();
 
-		if (savedState != null) {
-			year = savedState.getInt("year", c.get(Calendar.YEAR));
-			month = savedState.getInt("month", c.get(Calendar.MONTH));
-			day = savedState.getInt("day", Calendar.DAY_OF_MONTH);
-		} else if(year == -1 && month == -1 && day == -1) {
-			year = c.get(Calendar.YEAR);
-			month = c.get(Calendar.MONTH);
-			day = c.get(Calendar.DAY_OF_MONTH);
-		}
-		dateLabel = (TextView) getActivity().findViewById(R.id.dateLabel);
-	}
+        if (savedState != null) {
+            year = savedState.getInt("year", c.get(Calendar.YEAR));
+            month = savedState.getInt("month", c.get(Calendar.MONTH));
+            day = savedState.getInt("day", Calendar.DAY_OF_MONTH);
+        } else if (year == -1 && month == -1 && day == -1) {
+            year = c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH);
+            day = c.get(Calendar.DAY_OF_MONTH);
+        } else {
+            if (CommonActivity.service == null)
+                updateData();
+        }
+        dateLabel = (TextView) getActivity().findViewById(R.id.dateLabel);
+    }
 
-	protected void updateDate() {
-		// Oracle months are counted from zero instead of one
-		dateLabel.setText(String.format("%d.%d.%d", day, month + 1, year));
-	}
+    protected void updateDate() {
+        // Oracle months are counted from zero instead of one
+        dateLabel.setText(String.format("%d.%d.%d", day, month + 1, year));
+    }
 
-	public void updateData() {
-		ReservationListFragment listFragment = (ReservationListFragment) getFragmentManager().findFragmentByTag(ReservationListFragment.TAG);
-		listFragment.update(day, month + 1, year);
-	}
+    public void updateData() {
+        ReservationListFragment listFragment = (ReservationListFragment) getFragmentManager().findFragmentByTag(ReservationListFragment.TAG);
+        if (listFragment != null)
+            listFragment.update(day, month + 1, year);
+        else
+            Log.e(TAG, "Fragment not found!");
+    }
 
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		super.onCreateOptionsMenu(menu, inflater);
-		inflater.inflate(R.menu.reser_menu, menu);
-	}
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.reser_menu, menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.addReservation:
-			Log.d(TAG, "Add new booking time chosen");
-			Intent intent = new Intent(getActivity(), AddRecordActivity.class);
-			Bundle b = new Bundle();
-			b.putInt("year", year);
-			b.putInt("month", month);
-			b.putInt("day", day);
-			intent.putExtras(b);
-			startActivityForResult(intent, Constants.ADD_RECORD_FLAG);
-			break;
-		case R.id.refresh:
-			updateData();
-			Log.d(TAG, "Refresh data button pressed");
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addReservation:
+                Log.d(TAG, "Add new booking time chosen");
+                Intent intent = new Intent(getActivity(), AddRecordActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("year", year);
+                b.putInt("month", month);
+                b.putInt("day", day);
+                intent.putExtras(b);
+                startActivityForResult(intent, Constants.ADD_RECORD_FLAG);
+                break;
+            case R.id.refresh:
+                updateData();
+                Log.d(TAG, "Refresh data button pressed");
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	public void chooseDateClick(View v) {
-		Log.d(TAG, "Add new booking time chosen");
-		DatePickerDialog datePicker = new DatePickerDialog(getActivity(), dateSetListener, year, month, day);
-		datePicker.show();
-	}
+    public void chooseDateClick(View v) {
+        Log.d(TAG, "Add new booking time chosen");
+        DatePickerDialog datePicker = new DatePickerDialog(getActivity(), dateSetListener, year, month, day);
+        datePicker.show();
+    }
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		switch (requestCode) {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
             case (Constants.ADD_RECORD_FLAG): {
                 if (resultCode == Activity.RESULT_OK) {
                     ReservationListFragment listFragment = (ReservationListFragment) getFragmentManager().findFragmentByTag(ReservationListFragment.TAG);
@@ -152,17 +158,17 @@ public class AgendaListFragment extends Fragment implements OnClickListener {
                 }
                 break;
             }
-		}
-	}
+        }
+    }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.chooseDateButton:
-			chooseDateClick(v);
-			break;
-		}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.chooseDateButton:
+                chooseDateClick(v);
+                break;
+        }
 
-	}
+    }
 
 }
