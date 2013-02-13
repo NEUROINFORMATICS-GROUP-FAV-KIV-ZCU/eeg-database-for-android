0175e6bd467037dda97026566053222a3a32ac32
Petr Miko <miko.petr@gmail.com>
2013-02-13 17:48:01 +0100

    Edited view for uploading file, add file description text box - shows characters limit during user typing.

--- src/cz/zcu/kiv/eeg/mobile/base/ui/dash/ExperimentFragment.java
+++ src/cz/zcu/kiv/eeg/mobile/base/ui/dash/ExperimentFragment.java
@@ -5,12 +5,16 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import cz.zcu.kiv.eeg.mobile.base.R;
 import cz.zcu.kiv.eeg.mobile.base.data.Values;
 import cz.zcu.kiv.eeg.mobile.base.ui.filechooser.FileChooserActivity;
@@ -34,6 +38,31 @@ public class ExperimentFragment extends Fragment implements View.OnClickListener
         super.onActivityCreated(savedInstanceState);
         Button fChooserButton = (Button) getActivity().findViewById(R.id.fchooserButton);
         fChooserButton.setOnClickListener(this);
+
+
+        final TextView descriptionTextCount = (TextView) getActivity().findViewById(R.id.datafile_description_count);
+
+        final TextWatcher datafileDescriptionWatcher = new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() > 0){
+                    descriptionTextCount.setVisibility(View.VISIBLE);
+                    descriptionTextCount.setText(getString(R.string.data_file_left) + (getResources().getInteger(R.integer.limit_datafile_description_chars) - s.length()));
+                }
+                else{
+                    descriptionTextCount.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        };
+        EditText descriptionText = (EditText) getActivity().findViewById(R.id.datafile_description);
+        descriptionText.addTextChangedListener(datafileDescriptionWatcher);
+
     }
 
     @Override
@@ -53,9 +82,8 @@ public class ExperimentFragment extends Fragment implements View.OnClickListener
         switch (requestCode) {
             case (Values.SELECT_FILE_FLAG): {
                 if (resultCode == Activity.RESULT_OK) {
-                    TextView fChooserPathView = (TextView) getActivity().findViewById(R.id.fchooserPathView);
                     String path = data.getExtras().getString(Values.FILE_PATH);
-                    fChooserPathView.setText(path);
+                    Toast.makeText(this.getActivity(), path, Toast.LENGTH_SHORT).show();
                 }
                 break;
             }
