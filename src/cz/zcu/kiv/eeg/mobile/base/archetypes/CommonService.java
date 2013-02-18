f8d73b9765323f9a8ccad8459a10a45c46a83162
Petr Miko <miko.petr@gmail.com>
2013-02-18 10:27:08 +0100

    Exception handling
    Added more detailed REST service exception handling

--- src/cz/zcu/kiv/eeg/mobile/base/archetypes/CommonService.java
+++ src/cz/zcu/kiv/eeg/mobile/base/archetypes/CommonService.java
@@ -1,44 +1,76 @@
 package cz.zcu.kiv.eeg.mobile.base.archetypes;
 
-import cz.zcu.kiv.eeg.mobile.base.data.Values;
-import cz.zcu.kiv.eeg.mobile.base.data.ServiceState;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import cz.zcu.kiv.eeg.mobile.base.R;
+import cz.zcu.kiv.eeg.mobile.base.data.ServiceState;
+import cz.zcu.kiv.eeg.mobile.base.data.Values;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 
 import java.lang.ref.SoftReference;
 
 public abstract class CommonService<T, U, V> extends AsyncTask<T, U, V> {
 
-	protected SoftReference<CommonActivity> activity;
+    protected SoftReference<CommonActivity> activity;
 
-	public CommonService(CommonActivity context) {
-		this.activity = new SoftReference<CommonActivity>(context);
-	}
-	
-	protected void setState(ServiceState state){
-		activity.get().changeProgress(state);
-	}
-	
-	protected void setState(ServiceState state, int messageCode){
+    public CommonService(CommonActivity context) {
+        this.activity = new SoftReference<CommonActivity>(context);
+    }
+
+    protected void setState(ServiceState state) {
+        activity.get().changeProgress(state);
+    }
+
+    protected void setState(ServiceState state, int messageCode) {
         activity.get().changeProgress(state, activity.get().getString(messageCode));
-	}
-	
-	protected void setState(ServiceState state, String message) {
-        activity.get().changeProgress(state, message);
-	}
-	
-	protected void setState(ServiceState state, Throwable error) {
-		//TODO more readable error handling
-        activity.get().changeProgress(state, error.getLocalizedMessage());
-	}
+    }
 
-    public void setActivity(CommonActivity activity){
+    protected void setState(ServiceState state, String message) {
+        activity.get().changeProgress(state, message);
+    }
+
+    protected void setState(ServiceState state, Throwable error) {
+
+        String message = error.getMessage() == null ? activity.get().getString(R.string.error_connection) : error.getMessage();
+        //TODO more detailed error handling
+        if (error instanceof RestClientException) {
+
+            if (error instanceof HttpClientErrorException) {
+                HttpStatus status = ((HttpClientErrorException) error).getStatusCode();
+
+                switch (status) {
+                    case SERVICE_UNAVAILABLE:
+                        message = activity.get().getString(R.string.error_http_503);
+                        break;
+                    case NOT_FOUND:
+                        message = activity.get().getString(R.string.error_http_404);
+                        break;
+                }
+            } else {
+
+                error = ((RestClientException) error).getRootCause();
+                message = error.getMessage() == null ? activity.get().getString(R.string.error_connection) : error.getMessage();
+                if (message.contains("EHOSTUNREACH"))
+                    message = activity.get().getString(R.string.error_host_unreach);
+                else if (message.contains("ECONNREFUSED"))
+                    message = activity.get().getString(R.string.error_host_con_refused);
+                else if (message.contains("ETIMEDOUT"))
+                    message = activity.get().getString(R.string.error_host_timeout);
+            }
+
+        }
+
+        activity.get().changeProgress(state, message);
+    }
+
+    public void setActivity(CommonActivity activity) {
         this.activity = new SoftReference<CommonActivity>(activity);
     }
 
-	protected SharedPreferences getCredentials() {
-		return activity.get().getSharedPreferences(Values.PREFS_CREDENTIALS, Context.MODE_PRIVATE);
-	}
+    protected SharedPreferences getCredentials() {
+        return activity.get().getSharedPreferences(Values.PREFS_CREDENTIALS, Context.MODE_PRIVATE);
+    }
 }
--- src/cz/zcu/kiv/eeg/mobile/base/ws/TestCredentials.java
+++ src/cz/zcu/kiv/eeg/mobile/base/ws/TestCredentials.java
@@ -1,97 +1,88 @@
 package cz.zcu.kiv.eeg.mobile.base.ws;
 
-import static cz.zcu.kiv.eeg.mobile.base.data.ServiceState.*;
-
-import java.util.Collections;
-
-import cz.zcu.kiv.eeg.mobile.base.data.Values;
-import org.springframework.http.HttpAuthentication;
-import org.springframework.http.HttpBasicAuthentication;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.xml.SimpleXmlHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import cz.zcu.kiv.eeg.mobile.base.R;
-import cz.zcu.kiv.eeg.mobile.base.archetypes.CommonActivity;
-import cz.zcu.kiv.eeg.mobile.base.archetypes.CommonService;
-import cz.zcu.kiv.eeg.mobile.base.ui.NavigationActivity;
-import cz.zcu.kiv.eeg.mobile.base.ws.data.UserInfo;
-import cz.zcu.kiv.eeg.mobile.base.ws.ssl.HttpsClient;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
+import cz.zcu.kiv.eeg.mobile.base.R;
+import cz.zcu.kiv.eeg.mobile.base.archetypes.CommonActivity;
+import cz.zcu.kiv.eeg.mobile.base.archetypes.CommonService;
+import cz.zcu.kiv.eeg.mobile.base.data.Values;
+import cz.zcu.kiv.eeg.mobile.base.ui.NavigationActivity;
+import cz.zcu.kiv.eeg.mobile.base.ws.data.UserInfo;
+import cz.zcu.kiv.eeg.mobile.base.ws.ssl.HttpsClient;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.xml.SimpleXmlHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+import static cz.zcu.kiv.eeg.mobile.base.data.ServiceState.*;
 
 public class TestCredentials extends CommonService<Void, Void, Boolean> {
 
-	private final static String TAG = TestCredentials.class.getSimpleName();
+    private final static String TAG = TestCredentials.class.getSimpleName();
+    private boolean startupTest;
 
-	private boolean startupTest;
+    public TestCredentials(CommonActivity activity, boolean startupTest) {
+        super(activity);
+        this.startupTest = startupTest;
+    }
 
-	public TestCredentials(CommonActivity activity, boolean startupTest) {
-		super(activity);
-		this.startupTest = startupTest;
-	}
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        SharedPreferences credentials = getCredentials();
+        String username = credentials.getString("tmp_username", null);
+        String password = credentials.getString("tmp_password", null);
+        String url = credentials.getString("tmp_url", null) + Values.SERVICE_USER + "login";
 
-	@Override
-	protected Boolean doInBackground(Void... params) {
-		SharedPreferences credentials = getCredentials();
-		String username = credentials.getString("tmp_username", null);
-		String password = credentials.getString("tmp_password", null);
-		String url = credentials.getString("tmp_url", null) + Values.SERVICE_USER + "login";
+        setState(RUNNING, R.string.working_ws_credentials);
+        HttpAuthentication authHeader = new HttpBasicAuthentication(username, password);
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setAuthorization(authHeader);
+        requestHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
+        HttpEntity<Object> entity = new HttpEntity<Object>(requestHeaders);
 
-		setState(RUNNING, R.string.working_ws_credentials);
-		HttpAuthentication authHeader = new HttpBasicAuthentication(username, password);
-		HttpHeaders requestHeaders = new HttpHeaders();
-		requestHeaders.setAuthorization(authHeader);
-		requestHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
-		HttpEntity<Object> entity = new HttpEntity<Object>(requestHeaders);
-		
-		RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(HttpsClient.getClient()));
-		restTemplate.getMessageConverters().add(new SimpleXmlHttpMessageConverter());
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(HttpsClient.getClient()));
+        restTemplate.getMessageConverters().add(new SimpleXmlHttpMessageConverter());
 
-		try {
-			// Make the network request
-			Log.d(TAG, url);
-			ResponseEntity<UserInfo> userInfo = restTemplate.exchange(url, HttpMethod.GET, entity, UserInfo.class);
-			Values.user = userInfo.getBody();
-			return true;
-		} catch (Exception e) {
-			Log.e(TAG, e.getLocalizedMessage(), e);
-			setState(ERROR, e);
-			return false;
-		} finally {
-			setState(DONE);
-		}
-	}
+        try {
+            // Make the network request
+            Log.d(TAG, url);
+            ResponseEntity<UserInfo> userInfo = restTemplate.exchange(url, HttpMethod.GET, entity, UserInfo.class);
+            Values.user = userInfo.getBody();
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, e.getLocalizedMessage(), e);
+            setState(ERROR, e);
+            return false;
+        } finally {
+            setState(DONE);
+        }
+    }
 
-	@Override
-	protected void onPostExecute(Boolean verified) {
-		SharedPreferences credentials = getCredentials();
-		if (verified) {
-			String username = credentials.getString("tmp_username", null);
-			String password = credentials.getString("tmp_password", null);
-			String url = credentials.getString("tmp_url", null);
+    @Override
+    protected void onPostExecute(Boolean verified) {
+        SharedPreferences credentials = getCredentials();
+        if (verified) {
+            String username = credentials.getString("tmp_username", null);
+            String password = credentials.getString("tmp_password", null);
+            String url = credentials.getString("tmp_url", null);
 
-			SharedPreferences.Editor editor = credentials.edit();
-			editor.putString("username", username);
-			editor.putString("password", password);
-			editor.putString("url", url);
-			editor.commit();
+            SharedPreferences.Editor editor = credentials.edit();
+            editor.putString("username", username);
+            editor.putString("password", password);
+            editor.putString("url", url);
+            editor.commit();
 
-			Toast.makeText(activity.get(), R.string.settings_saved, Toast.LENGTH_SHORT).show();
+            Toast.makeText(activity.get(), R.string.settings_saved, Toast.LENGTH_SHORT).show();
 
-			if (startupTest)
-				activity.get().startActivity(new Intent(activity.get(), NavigationActivity.class));
-			else
-				activity.get().finish();
-		}
-	}
+            if (startupTest)
+                activity.get().startActivity(new Intent(activity.get(), NavigationActivity.class));
+            else
+                activity.get().finish();
+        }
+    }
 
 }
